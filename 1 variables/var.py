956bc6f09727573d53a2cd6f5c196f8a67657100
omkar <shelkeom230@gmail.com>
2024-03-07 14:46:13 +0530

    1 variables

---
+++ 1 variables/var.py
@@ -0,0 +1,35 @@
+rent=1700
+petrol=200
+groceries=3000
+# print(f"I pay rent of {rent} Rs\nI use petrol worth {petrol} Rs and\nI need monthly groceries of {groceries} Rs")
+
+print(f"total is {rent+petrol+groceries}")
+
+gabbar='amjad khan'
+jay='amitabh'
+thakur='sanjiv kumar'
+print("actors in sholey: ",gabbar,jay,thakur)
+
+print(type(rent),type(gabbar))
+
+# boolean 
+isAllNum=False
+learn_python=True 
+
+# var naming rules 
+# 1. no keywords
+# 2. no special chars
+# 3. they just begin with letter or _
+# 4. other char may be letters or numbers 
+# 5. names are case sensitive 
+
+bade_bhai=10
+bade_Bhai=5
+print(bade_bhai,bade_Bhai)
+
+foo=5
+foo="jalebi"
+bar=foo
+
+print("bar id: ",id(bar))
+print("foo id : ",id(foo))
